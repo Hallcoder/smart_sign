6eb73d198d205008119777fa0734094c8a89e69a
Hallcoder <hallcoder25@gmail.com>
2022-09-02 12:44:15 +0200

    options page  and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import logo from '../assets/images/logo.png';
 import Input from './common/input';
-function NavBar() {
+import {MdSearch} from 'react-icons/md';
+function NavBar({title}) {
     return ( 
-        <div className='fixed top-0 z-1 flex w-full justify-between'>
+        <div className='fixed top-0 z-1 flex w-full items-center justify-between border-b shadow-md'>
             <div>
-        <img src={logo} alt="logo" className='w-16 h-14' />
+           <img src={logo} alt="logo" className='w-16 h-14 m-2' />
+            </div>
+            <div className='text-xl font-bold'>
+                {title}
             </div>
             <div className='flex items-center mr-1'>
                 <button className='bg-blue-800 h-12 rounded-md m-1 w-20 text-white font-bold text-xl'>New</button>
                 <Input type={'text'} placeholder={'Find a permission...'} name={'searchValue'}/>
+                <MdSearch className='text-2xl m-2 w-2/12 text-gray-600 cursor-pointer h-4/6' />
             </div>
         </div>
      );
--- src/pages/options.jsx
+++ src/pages/options.jsx
@@ -9,9 +9,9 @@ import NavBar from '../components/Navbar';
 function Options() {
     return ( 
         <div>
-            <NavBar />
-            <div>
-                
+            <NavBar title={'Options Page'}/>
+            <div className='w-6/12 border border-blue-400 mt-60 h-[40vh] rounded-md m-auto'>
+                <h1 className='m-auto text-xl text-center'>Choose what you want to do:</h1>
             </div>
         </div>
      );

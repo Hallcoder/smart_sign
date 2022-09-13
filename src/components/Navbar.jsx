df46de3c108d1d01ddfffbd871f8d3b53019ab59
Hallcoder <hallcoder25@gmail.com>
2022-09-13 18:43:03 +0200

    tried to implement the search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import logo from '../assets/images/logo.png';
 import Input from './common/input';
 import intro from '../assets/images/intro.jpg';
 import {MdSearch,MdPersonPin,MdMenu} from 'react-icons/md';
+import Search from './search';
 function NavBar({title}) {
     return ( 
         <div className='fixed top-0 z-1 h-[8vh] bg-white flex w-full items-center justify-between border-b shadow-md'>
@@ -14,8 +15,8 @@ function NavBar({title}) {
             </div>
             <div className='none items-center mr-1 hidden sm:flex'>
                 <button className='bg-blue-800 h-12 rounded-md m-1 w-20 text-white font-bold text-xl'>New</button>
-                <Input type={'text'} placeholder={'Find a permission...'} name={'searchValue'}/>
-                <MdSearch className='text-2xl m-2 w-2/12 text-gray-600 cursor-pointer h-4/6' />
+                <Search />
+                {/* <MdSearch className='text-2xl m-2 w-2/12 text-gray-600 cursor-pointer h-4/6' /> */}
                 <MdPersonPin className='text-5xl cursor-pointer'/>
             </div>
             <div className='sm:hidden flex'>

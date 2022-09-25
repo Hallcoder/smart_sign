f885b063e0306850031b063aeacb0584e644067f
Hallcoder <hallcoder25@gmail.com>
2022-09-25 13:46:37 +0200

    env changed and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,10 @@ import Input from './common/input';
 import intro from '../assets/images/intro.jpg';
 import {MdSearch,MdPersonPin,MdMenu} from 'react-icons/md';
 import axios from 'axios';
-import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+import { InstantSearch, SearchBox,Configure ,connectHits} from 'react-instantsearch-dom';
 import algoliasearch from 'algoliasearch';
 function NavBar({title}) {
-    const searchClient = algoliasearch(import.meta.env.ALGOLIA_APPID, import.meta.env.ALGOLIA_ADMINKEY,{
+    const searchClient = algoliasearch(import.meta.env.VITE_ALGOLIA_APPID, import.meta.env.VITE_ALGOLIA_ADMINKEY,{
         enablePersonalization:false
     })
     const index = searchClient.initIndex('permissions')
@@ -20,8 +20,16 @@ function NavBar({title}) {
     //           })
     //        })
     // },[])
-
-
+    const Hits = ({ hits }) => (
+        <ol className='border-2'>
+          {hits.map(hit => (
+            <div key={hit.objectID} className='border-b border-black'>
+            <p>{hit.name}</p>
+            </div> 
+          ))}
+        </ol>
+      );      
+    const CustomHits = connectHits(Hits);
     return ( 
         <div className='fixed top-0 z-1 h-[8vh] bg-white flex w-full items-center justify-between border-b shadow-md'>
             <div>
@@ -33,13 +41,20 @@ function NavBar({title}) {
             <div className='none items-center mr-1 hidden sm:flex'>
                 <button className='bg-blue-800 h-12 rounded-md m-1 w-20 text-white font-bold text-xl'>New</button>
                 {/* <Input></Input> */}
-                <InstantSearch
+            <div className='mt-36'>
+               <InstantSearch
     indexName={index.indexName}
     searchClient={searchClient}
   >
-    <SearchBox />
-    <Hits />
+<SearchBox />
+<Configure
+  hitsPerPage={4}
+  enablePersonalization={false}
+  distinct
+/>
+<CustomHits />
   </InstantSearch>
+               </div>
                 <MdSearch className='text-2xl m-2 w-2/12 text-gray-600 cursor-pointer h-4/6' />
                 <MdPersonPin className='text-5xl cursor-pointer'/>
             </div>
